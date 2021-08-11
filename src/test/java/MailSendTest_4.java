248f4eca532f4f3dee8a80de2375a5e39891f891
Руслан <testselenide@mail.ru>
2021-08-11 08:59:48 +0500

    First commit

--- src/test/java/MailSendTest_4.java
+++ src/test/java/MailSendTest_4.java
@@ -1,3 +1,5 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.commands.WaitUntil;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import java.time.LocalDateTime;
@@ -8,14 +10,14 @@ import static com.codeborne.selenide.Selenide.*;
 public class MailSendTest_4 {
     @Test
     void mailSendTest() {
+        Configuration.timeout=20000;
         open("https://mail.ru/");
 
         mailAuth("testselenide", "javauicheckup");
         mailSend("bikmaevrr@yandex.ru", LocalDateTime.now() + " Тестовое сообщение", "Test message");
     }
 
-    void mailAuth(String user, String pass)
-    {
+    void mailAuth(String user, String pass) {
         $(By.name("login")).shouldBe(visible).setValue(user);
         $(".button.svelte-1eyrl7y").shouldBe(visible).click();
         $(By.name("password")).shouldBe(visible).setValue(pass);
@@ -27,7 +29,8 @@ public class MailSendTest_4 {
         $(".scrollview--SiHhk.scrollview_main--3Vfg9").shouldBe(visible);
         $(".container--H9L5q.size_s--3_M-_").shouldBe(visible).setValue(sendTo);
         $(By.name("Subject")).shouldBe(visible).setValue(subject);
-        $x("/html/body/div[15]/div[2]/div/div[1]/div[2]/div[3]/div[5]/div/div/div[2]/div[1]/div[1]").shouldBe(visible).setValue(messageText);
+        $x("/html/body/div[15]/div[2]/div/div[1]/div[2]/div[3]/div[5]/div/div/div[2]/div[1]/div[1]")
+                .shouldBe(visible).setValue(messageText);
         $("span.button2.button2_base.button2_primary.button2_hover-support.js-shortcut").shouldBe(visible).click();
     }
 
