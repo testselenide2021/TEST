5631ec5933f507ad1b84302d07ef2ce9245de762
testselenide2021 <88710605+testselenide2021@users.noreply.github.com>
2021-08-13 18:21:14 +0500

    Update JetbrainsHotkeyCheck.java

--- src/test/java/JetbrainsHotkeyCheck.java
+++ src/test/java/JetbrainsHotkeyCheck.java
@@ -15,7 +15,7 @@ public class JetbrainsHotkeyCheck {
         ElementsCollection hotKeys = $$(".wt-code.wt-code_theme_light");
         ElementsCollection aboutKeys = $$(".wt-text-2.wt-text-2_hardness_average.wt-text-2_theme_light.shortcuts-info__legend");
 
-            hotKeys.get(5).shouldBe(visible).shouldHave(text("Ctrl + Shift + A"));
+            hotKeys.get(5).shouldBe(visible).shouldHave(text("Ctrl + Shift + A1"));
             hotKeys.get(6).shouldBe(visible).shouldHave(text("Alt + Enter"));
             hotKeys.get(7).shouldBe(visible).shouldHave(text("Alt + Insert"));
             hotKeys.get(8).shouldBe(visible).shouldHave(text("Alt + F7"));
